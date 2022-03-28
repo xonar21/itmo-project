c2388f37f3f134907e6cec0994ca171a7c7c4d97
Spynu Jean <jspynu14@mail.ru>
2022-03-28 15:19:19 +0400

    slider

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,268 +1,123 @@
-import './index.css';
+const windowOuterWidth = window.outerWidth
+console.log(windowOuterWidth)
 
-import Api from '../components/Api.js';
-import Card from '../components/Card.js';
-import FormValidator from '../components/FormValidator.js';
-import Section from '../components/Section.js';
-import PopupWithImage from '../components/PopupWithImage.js';
-import PopupWithForm from '../components/PopupWithForm.js';
-import PopupDelete from '../components/PopupDelete.js';
-import UserInfo from '../components/UserInfo.js';
-
-import {
-  submitDelete,
-  submitAddCard,
-  submitAvatar,
-  submitProfile,
-  cardListSelector,
-  popupProfile,
-  deletePopup,
-  popupAdd,
-  popupImg,
-  editBtn,
-  imagePopup,
-  imagepopUptitle, 
-  addBtn,
-  cardTemplate,
-  info,
-  config,
-  nameInput,
-  subnameInput,
-  ava,
-  avatarPopup,
-  formValidators} from '../utils/constants.js';
-
-export const api = new Api({
-  baseUrl: 'https://mesto.nomoreparties.co/v1/cohort-35/',
-  headers: {
-    authorization: '4f1e5520-cc86-4d96-8418-3f1ecec3cfa5',
-    'Content-Type': 'application/json'
-  }
-});
-
-const userInfo = new UserInfo(info);
-
-let cardsItem = [];
-let cardsList = null;
-
-
-//просмотр картинки
-const popupView = new PopupWithImage(popupImg);
-
-
-//удаление
-const openDeleteCard = new PopupDelete(
-  deletePopup,
-  (card) => { delCard(card); });
-
-//создание карточки
-function createCard(item) {
-  const card = new Card(
-    {
-      data: item,
-      del: (card) => { openDeleteCard.open(card); },
-      handleLike: (card) => { likeCard(card); }
-    },
-     
-    cardTemplate, 
-    handleCardClick
-
-  );
-  const cardElement = card.generateCard(userInfo.getUserID());
-  return cardElement // возваращаете готовую карточку
+let offset = 0;
+let current = 0;
+let left = 0
+const sliderLine = document.querySelector('.aboutUs__cards');
+const card = document.querySelectorAll('.aboutUs__card');
+const dots = document.querySelectorAll('.aboutUs__point');
+const length = dots.length - 1;
+dots[current].classList.add('aboutUs__point_active')
+if (left > 0) {
+  offset = offset + 1148;
 }
-  
-
-
-// лайки
-const likeCard = (card) => {
-  const id = card.getCardId();
-  const user = userInfo.getUserID();
-  const likeState = card.isLiked();
-  const action = likeState ? 'удалить' : 'поставить';
-  const likeFunc = likeState
-    ? (cardId) => api.unlikeCard(cardId)
-    : (cardId) => api.likeCard(cardId);
-  
-
-  likeFunc(id)
-    .then((res) => {
-      card.setLikes(res.likes);
-      card.setLikeGroup(user);
-    })
-    .catch((err) => {
-      console.log(`Невозможно ${action} лайк. Ошибка ${err}.`);
-      card.setLikes(!likeState ? [{ _id: user }] : []);
-    });
-}
-
-//удаление карточки
-const delCard = (card) => {
-  submitDelete.textContent = 'Сохранение...';
-  api.delCardFromServer(card.getCardId())
-    .then((res) => {
-      card.del();
-      openDeleteCard.close();
-    })
-    .catch((err) => {
-      console.log(`Невозможно удалить карточку. Ошибка ${err}.`);
-    })
-    .finally(() => {
-      
-      submitDelete.textContent = 'Сохранить';
-    });
-  
-};
-
-//добавление карточки
-const openAddCard = new PopupWithForm( 
-  popupAdd, 
-  (item) => {
-    submitAddCard.textContent = 'Сохранение...';
-    api.postCard({name: item.title, link: item.link})
-      .then((res) => {
-        
-        
-        cardsList.addItem(createCard({
-          name: res.name, 
-          link: res.link, 
-          likes: res.likes, 
-          owner: res.owner._id,
-          id: res._id
-        }));
-        
-        openAddCard.close();
-       
-      })
-      .catch((err) => {
-        console.log(`Невозможно сохранить карточку на сервере. Ошибка ${err}.`);
-      })
-      .finally(() => {
-        submitAddCard.textContent = 'Сохранить';
-      });
-  }
-);
-
-
-//редактирование аватара
-const openAvatar = new PopupWithForm(
-  avatarPopup,
-  (data) => { saveAvatar(data); });
-
-function saveAvatar(data) {
-    submitAvatar.textContent = 'Сохранение...';
-    api.patchAvatar(data)
+sliderLine.style.left = -offset + 'px';
+document.querySelector('.aboutUs__arrow-right').addEventListener('click', function(){
     
-      .then((res) => {
-        userInfo.setUserAvatar(res.avatar);
-        userInfo.setUserId(res._id);
-        openAvatar.close();
-      })
-      .catch((err) => {
-        console.log(`Невозможно обновить аватар на сервере. ${err}.`);
-      })
-      .finally(() => {
-        submitAvatar.textContent = 'Сохранить';
-      });
+  if(offset == 2296) {
+    offset = offset + 385;
+  } else {
+    offset = offset + 1148;
   }
 
-  //редактирование профиля
-const openProfile = new PopupWithForm(popupProfile,
-  (data) => { saveUserProfileServer(data); });
+    if (offset > 2681) {
+        offset = 0;
+    }
+    sliderLine.style.left = -offset + 'px';
+    
 
-function saveUserProfileServer(Data) {
-  submitProfile.textContent = 'Сохранение...';
-  api.pathEditProfile(Data)
-    .then((res) => {
-      console.log(res._id)
-      userInfo.setUserInfo({ name: res.name, subname: res.about });
-      userInfo.setUserId(res._id);
-      openProfile.close();
-    })
-    .catch((err) => {
-      console.log(`Невозможно обновить профиль пользователя. ${err}.`);
-    })
-    .finally(() => {
-      submitProfile.textContent = 'Сохранить';
-    });
+    if(current == length) {
+      dots[current].classList.remove('aboutUs__point_active')
+      current = 0
+      dots[current].classList.add('aboutUs__point_active')
+    } else {
+      dots[current].classList.remove('aboutUs__point_active')
+      current = current + 1
+      dots[current].classList.add('aboutUs__point_active')
+      //console.log(current, dots.length)
+    }
+    
+});
+
+document.querySelector('.aboutUs__arrow-left').addEventListener('click', function () {
+    
+    if(offset == 385) {
+      offset = 0
+    } else {
+      offset = offset - 1148;
+    }
+    if (offset < 0) {
+        offset = 2681;
+    }
+    sliderLine.style.left = -offset + 'px';
+
+    if(current == 0) {
+      dots[current].classList.remove('aboutUs__point_active')
+      current = current + length;
+      dots[current].classList.add('aboutUs__point_active')
+    }
+    else {
+      dots[current].classList.remove('aboutUs__point_active')
+      current--;
+      dots[current].classList.add('aboutUs__point_active')
+    }
+});
+let x1 = null;
+let y1 = null;
+
+function handleTouchStart(event) {
+  const firstTouch = event.touches[0]
+  x1 = firstTouch.clientX;
+  y1 = firstTouch.clientY;
+
 }
 
+function handleTouchMove(event) {
+  if (x1 == null || y1 == null) {
+    return false;
+  }
 
-function handleCardClick(name, link, alt) {
-  imagePopup.src = link;
-  imagePopup.alt = alt;
-  imagepopUptitle.textContent = name;
-  popupView.open({name,link});
-}
-
-
-//валидация
-  const enableValidation = (config) => {
-    const formList = Array.from(document.querySelectorAll(config.formSelector));
-    formList.forEach((formElement) => {
-      const validator = new FormValidator(formElement, config);
-      const formName = formElement.getAttribute('name');
-      formValidators[formName] = validator;
-      validator.enableValidation();
-    });
-  };
-
-//слушатели
-openProfile.setEventListeners();
-openAddCard.setEventListeners();
-popupView.setEventListeners();
-openDeleteCard.setEventListeners();
-openAvatar.setEventListeners();
-
-editBtn.addEventListener('click', () => {
-  const userData = userInfo.getUserInfo();
-  nameInput.value = userData.name;
-  subnameInput.value = userData.subname;
-  openProfile.open();
-  formValidators.edit.resetValidation();
-});
-addBtn.addEventListener('click',() => {
-  openAddCard.open();
-  formValidators.add.resetValidation();
-});
-
-ava.addEventListener('click', () => {
-  const avatar = userInfo.getUserAvatar();
-  openAvatar.open({ avatar });
-});
-
-enableValidation(config);
-
-
-//данные с сервера
-  Promise.all([api.getUserInformation(), api.getCardsFromServer()])
-  .then(([userData, cards]) => {
-    userInfo.setUserInfo({name: userData.name, subname: userData.about});
-    userInfo.setUserAvatar(userData.avatar);
-    userInfo.setUserId(userData._id);
-
-    cardsItem = cards.map(item => {
-      return {
-        name: item.name,
-        likes: item.likes,
-        link: item.link,
-        id: item._id,
-        owner: item.owner._id
-        }
-    });
-
-    cardsList = new Section(
-      { data: cardsItem, renderer: (item) => cardsList.addItem(createCard(item))}, 
-      cardListSelector);
-      cardsList.renderItems();
-
-      
-  })
-  .catch(err => {
-    console.log(`Информация с сервера не получена.${err}.`);
-  });
-
- 
-
- 
+  let x2 = event.touches[0].clientX;
+  let y2 = event.touches[0].clientY;
+  let xDiff = x2 - x1;
+  let yDiff = y2 - y1;
   
+  if (Math.abs(xDiff) > Math.abs(yDiff)) {
+    if (xDiff > 0) {
+      offset = offset - 1148;
+      sliderLine.style.left = -offset + 'px';
+      
+    }
+    else {
+      offset = offset + 1148;
+      sliderLine.style.left = -offset + 'px';
+      
+    } 
+  }
+  else {
+    if (yDiff > 0) {
+      console.log('down')
+    } 
+    else {
+      console.log('top')
+    } 
+  }
+  x1 = null;
+  y1 = null;
+
+}
+
+sliderLine.addEventListener('touchstart', handleTouchStart);
+sliderLine.addEventListener('touchmove', handleTouchMove);
+// мобильное меню
+const burgerMenu = document.querySelector('.header__buttonMenu');
+const menu = document.querySelector('.header__menu');
+const exit = document.querySelector('.header__exit');
+
+burgerMenu.addEventListener('click', () => {
+  menu.classList.add('header__menu_active')
+})
+exit.addEventListener('click', () => {
+  menu.classList.remove('header__menu_active')
+})
