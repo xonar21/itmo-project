a8724cd625d23543cbd54952190504070c969640
Eldar <eldar0788@gmail.com>
2022-04-03 17:02:25 +0500

    feat: добавить попап-поделиться. добавить библиотеку likely

--- src/scripts/publications.js
+++ src/scripts/publications.js
@@ -1,22 +1,30 @@
-import { publications, cards, cardTemplate } from "./constance.js";
+import {publications, cards, cardTemplate} from "./constance.js";
 
 function renderCards(publications) {
-  publications.forEach((card) => {
-    cards.append(createCard(card));
-  });
+    publications.forEach((card) => {
+        cards.append(createCard(card));
+    });
 }
 
 function createCard(publication) {
-  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
-  const cardElementImage = cardElement.querySelector(".card__img");
-  cardElementImage.src = publication.image;
-  cardElementImage.alt = publication.title;
-  cardElement.querySelector(".card__title").textContent = publication.title;
-  cardElement.querySelector(".card__authors").textContent = publication.authors;
-  cardElement.querySelector(".card__description").textContent =
-    publication.text;
-  // setEventListeners();
-  return cardElement;
+    const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+    const cardElementImage = cardElement.querySelector(".card__img");
+    const cardElementPopup = cardElement.querySelector(".card__sharePopup");
+    const cardElementShareIcon = cardElement.querySelector(".card__icon");
+    cardElementImage.src = publication.image;
+    cardElementImage.alt = publication.title;
+    cardElement.querySelector(".card__title").textContent = publication.title;
+    cardElement.querySelector(".card__authors").textContent = publication.authors;
+    cardElement.querySelector(".card__description").textContent = publication.text;
+    // setEventListeners();
+    cardElementShareIcon.addEventListener('click', () => {
+        cardElementPopup.classList.add('active');
+        cardElementPopup.addEventListener('click', () => {
+            cardElementPopup.classList.remove('active');
+        });
+    });
+
+    return cardElement;
 }
 
 renderCards(publications);
